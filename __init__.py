65d1887475e9877872ffbbfbf6faf074e049be1c
GRETETE Karim <gretetekarim@hotmail.fr>
2012-07-02 20:29:37 +0200

    init : clean up

--- __init__.py
+++ __init__.py
@@ -47,25 +47,7 @@ except:
     print(misc.ConsoleError("Import external module ", 0, False))
 
 #Globals
-misc.LogError("", True)
-misc.LogError("*"*78, True)
-misc.LogError("*" + " "*22 + "Shader Tools Next Gen - Console" + " "*23 + "*", False)
-misc.LogError("*"*78, False)
-blender_version = str(bpy.app.version[0]) + "." + str(bpy.app.version[1]) + str(bpy.app.version[2])
-default_paths = environment.DefaultPaths()
-api_functions = environment.ApiDatas(default_paths['apis_database'], blender_version)
-configurations_config = environment.ConfigurationsDatas(default_paths['configs_database'], False)
-languages_config = environment.LanguagesDatas(default_paths['languages_database'])
-active_configuration = environment.ActiveConfigurations(configurations_config)
-active_languages = environment.ActiveLanguage(languages_config, active_configuration['language'])
-about_config = environment.AboutDatas(default_paths['database'])
-active_categories = environment.MaterialsCatergories(active_languages)
-names_config = environment.ConfigurationsNames(configurations_config)
-options_actions = environment.ConfigurationsOptions(active_languages)
-names_languages = environment.LanguagesNames(languages_config)
-space_access_name = active_languages['space_access_name'] + " "
-print(misc.ConsoleError("Globals ", 0, True))
-'''try:
+try:
     misc.LogError("", True)
     misc.LogError("*"*78, True)
     misc.LogError("*" + " "*22 + "Shader Tools Next Gen - Console" + " "*23 + "*", False)
@@ -85,7 +67,7 @@ print(misc.ConsoleError("Globals ", 0, True))
     space_access_name = active_languages['space_access_name'] + " "
     print(misc.ConsoleError("Globals ", 0, True))
 except:
-    print(misc.ConsoleError("Globals ", 0, False))'''
+    print(misc.ConsoleError("Globals ", 0, False))
 #Functions
 conf_current_name = ""
 conf_current_idx = 1
