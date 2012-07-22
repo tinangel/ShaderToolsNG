9f9650e1097cad83e5132b708fd84a8f66e7281f
GRETETE Karim (tinangel) <gretetekarim@hotmail.fr>
2012-07-22 14:20:15 +0200

    Migation : include load bar in panel (clean up)

--- __init__.py
+++ __init__.py
@@ -100,8 +100,7 @@ def LoadingMigrateProgressBar(path):
     misc.LogAndPrintError(("*"*78,  "*"*78))
     misc.LogAndPrintError(("Database version : %s" %version_values[2] ,  "Database version : %s" %version_values[2]))
     misc.SaveDatabase(default_paths['database'],  default_paths['save'],  default_paths['bin'])
-    for v in range(2, 50):
-    #for v in range(2, number_max[0]+1):
+    for v in range(2, number_max[0]+1):
         ctx_scene.shadertoolsng_utils_bar = misc.CrossProduct(v+1, 50)
         err = active_languages['menu_error_error037'] % str(v)
         print("\n%s" % err)
